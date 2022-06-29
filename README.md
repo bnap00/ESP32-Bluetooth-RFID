9f13259c0030803b108fdd588b23656ffa34556b
Bharat Parsiya <bnap00@gmail.com>
2022-06-29 19:50:55 +0530

    migrate from I2C to SPI

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # RFID-Scanner
 
+*For Some reason I was not able to get [Original Repo](https://github.com/MariusVB/ESP32-Bluetooth-RFID) to work with my ESP32 varient. So I repurposed the code from original repo and used refrences from [this tutorial](https://esp32io.com/tutorials/esp32-rfid-nfc) to create a SPI based project I2C based project*
+
 Project to utilize an RFC522 RFID Scanner with an ESP32 to act as a Bluetooth RFID Scanner. Set up as a PlatformIO project.
 
-
-Thanks to @semaf for the [MFRC522 I2C Library](https://github.com/semaf/MFRC522_I2C_Library)
-
 Thanks to @T-vk for the [ESP32-BLE-Keyboard library](https://github.com/T-vK/ESP32-BLE-Keyboard)
+
+Thanks to @semaf for the [MFRC522 I2C Library](https://github.com/semaf/MFRC522_I2C_Library) *(Not using this library anymore, but hey, keepup the good work)*
+
+![Wiring Diagram](wiring-diagram.jpg)
+_wiring diagram from [Tutorial website](https://esp32io.com/tutorials/esp32-rfid-nfc). Incase website goes away. Let me know if you want me to take down the image._
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <BleKeyboard.h>
 #include <Arduino.h>
-#include <MFRC522_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
 // Declare variables
-#define RST 14
-#define SDA 23
-#define SCL 22
 #define DEVICE_NAME "RFID Scanner"
 #define READ_TIMEOUT_MS 3000
+#define SS_PIN  5  // ESP32 pin GIOP5 
+#define RST_PIN 27 // ESP32 pin GIOP27 
 
 // Declare scanner
-MFRC522 mfrc522(0x28, RST);
+// MFRC522 mfrc522(0x28, RST);
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
 // Declare keyboard with Bluetooth device name
-BleKeyboard bleKeyboard("RFID Scanner");
+BleKeyboard bleKeyboard("TSS RFID Scanner");
 
 // Forward function declaration
 String getRFIDString(MFRC522::Uid);
@@ -20,21 +23,19 @@ String getRFIDString(MFRC522::Uid);
 void setup() {
     Serial.begin(115200);
     bleKeyboard.begin();
-
-    // Begin SDA and SCL Wire
-    Wire.begin(SDA,SCL);
-    // Init Scanner
-    mfrc522.PCD_Init();
+    SPI.begin();
+    rfid.PCD_Init();
+    Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
 }
 
 int lastRead = 0;
 void loop() {
 
-    bool cardRead = mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial();
+    bool cardRead = rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial();
     bool timeoutOver = (lastRead + READ_TIMEOUT_MS) < millis();
     
     if (bleKeyboard.isConnected() && cardRead && timeoutOver ) {
-        String result = getRFIDString(mfrc522.uid);        
+        String result = getRFIDString(rfid.uid);        
         Serial.println("RFID: " + result);
         bleKeyboard.println(result);
         lastRead = millis();
@@ -52,7 +53,7 @@ String getRFIDString(MFRC522::Uid uid) {
         if (uid.uidByte[i] < 0x10) {
             result += "0";
         }
-        result += String(mfrc522.uid.uidByte[i], HEX);
+        result += String(rfid.uid.uidByte[i], HEX);
     }
     result.toUpperCase();
     return result;
